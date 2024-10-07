780660078ed5757f9b81e5bad5519687b7d3fa26
laura alice <lagglaura@gmail.com>
2024-10-07 19:07:00 -0300

    trabalha em dupla
     numero para escolher uma classe

---
+++ trabalhoemdupla.py
@@ -0,0 +1,10 @@
+#insira um número para escolher a classe:
+
+numero = int(input("digite um número:"))
+if numero==1:
+       print(f"o número{numero} pertence a classe barbaro.")
+elif numero==2:
+       print(f"o número {numero} pertence a classe mago.")
+elif numero==3:
+       print(f"o número {numero} pertence a classe arqueiro.")
+       
