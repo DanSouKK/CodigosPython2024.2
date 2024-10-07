1cfe8d8709a0ecc52cb63bae941e9bade8f92a08
DanSouKK <dansouzaofficial@gmail.com>
2024-10-07 19:46:04 -0300

    trabalhoemdupla.py
    Alguns ajustes e adição de escolha de equipamento.

--- trabalhoemdupla.py
+++ trabalhoemdupla.py
@@ -1,10 +1,40 @@
+#faca um programa que pede o usuario para escolhar uma classe
+#com base no numero que ele escolher, sendo:
+#1 - mago //2 - barbaro // 3 - arqueiro
 #insira um número para escolher a classe:
-
-numero = int(input("digite um número:"))
+print("Escolha sua classe para jogar!")
+print("1 - Barbaro!")
+print("2 - Mago!")
+print("3 - arqueiro!")
+#usuario ira digitar um numero para escolher sua classe abaixo
+numero = int(input("digite um número: "))
 if numero==1:
-       print(f"o número{numero} pertence a classe barbaro.")
+       classe="Barbaro"
 elif numero==2:
-       print(f"o número {numero} pertence a classe mago.")
+       classe="Mago"
 elif numero==3:
-       print(f"o número {numero} pertence a classe arqueiro.")
+       classe="Arqueiro"
+print("Muito bem ja escolheu sua classe!")
+
+#usuario ja escolheu sua classe
+#agora ele tera que escolher seus equipamentos
+print("Agora escolha qual tipo de equipamento irá usar!")
+print("1 - Machado")
+print("2 - Cajado")
+print("3 - Arco e Flecha")
+print("4 - Espada e Escudo")
+equip = int(input("Escolha seu equip.: "))
+if(equip==1):
+       estilo="Machado"
+elif(equip==2):
+       estilo="Cajado"
+elif(equip==3):
+       estilo="Arco e Flecha"
+elif(equip==4):
+       estilo="Espada e Escudo" 
+print("Você esta pronto para aventura!")
+print(f"Você jogara de {classe} e usará {estilo}")
+print("Hora da aventura!")
+
+
        
